2b00258f7e843c4fdf59bf35d512677459425521
Guilherme Bibiano <131410784+Menotso@users.noreply.github.com>
2024-07-30 12:41:24 -0300

    Delete FastAPI directory

--- FastAPI/load_classifier.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# carrega um classificador existente no diretório
-def load_classifier(modelo) -> pickle.load:
-    # classificador remodelado sendo substituido
-    with open(modelo, "rb") as f:
-        classifier = pickle.load(f)
-    return classifier
--- FastAPI/load_df.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pickle
-
-# carrega um dataframe
-def load_df() -> pd.DataFrame:
-    dataset_original = "EDA_df.pkl"
-    with open(dataset_original, "rb") as f:
-        df_original = pickle.load(f)
-    return df_original
